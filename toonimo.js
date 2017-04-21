dfa2ea9392e10c359d59bd46ab466ff2ca053337
Hanitale <atounh@gmail.com>
2017-04-21 18:26:08 +0300

    and style

--- toonimo.js
+++ toonimo.js
@@ -1,23 +1,30 @@
 $('button').click(function(){
-    var otherArray = [];
+    var tempArray = [];
     var divs = $('div');
     var divArray =$.makeArray(divs);
 
-     for(var i=0; i< divArray.length; i++){
-        otherArray.push( divArray[i].innerText);
+    for(var i=0; i< divArray.length; i++){
+        var nameStyle = {name:divArray[i].innerText,style:divArray[i].style.backgroundColor};
+        tempArray.push(nameStyle);
     }
-   otherArray.sort();
+ 
+    tempArray.sort(function(a, b){
+    var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
+    if (nameA < nameB) //sort string ascending
+        return -1
+    if (nameA > nameB)
+        return 1
+    return 0
+});
 
-   for(var i=0; i<otherArray.length; i++){
-       divArray[i].innerText = otherArray[i];
+   for(var i=0; i<divArray.length; i++){
+       divArray[i].style.backgroundColor = tempArray[i].style;
+       divArray[i].innerText = tempArray[i].name;
+
    }
-
-   var newHtml = "<h1>one moment please</h1>";
 
     $('div').remove();
     $('button').remove();
    $('body').append(divArray);
-
-
 });
 
